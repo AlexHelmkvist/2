a9f065d5db22d3c9f9d06d31bf883d1c32756785
Alex <alex33376@hotmail.com>
2024-09-19 09:58:23 +0200

    Added new unit testing class Deca100MTest

---
+++ src/test/java/decathlon/Deca100MTest.java
@@ -0,0 +1,11 @@
+package decathlon;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class Deca100MTest {
+
+    @Test
+    void calculateResult() {
+    }
+}
